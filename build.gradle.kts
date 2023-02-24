39105dd9f9d2e4a7e621c47bd7cfb27e4af9ce1f
Loïc Vanden Bossche <loloic066@gmail.com>
2023-02-24 19:05:20 +0100

    Added database connection to app

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,12 @@ tasks.getByName<Jar>("jar") {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
