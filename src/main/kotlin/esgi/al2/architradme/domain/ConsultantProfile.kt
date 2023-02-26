042ff4cd4868227a3de81e78069bf44f0fec42bd
Barlords <basile.pulin@gmail.com>
2023-02-26 02:07:06 +0100

    update ConsultantProfile

--- src/main/kotlin/esgi/al2/architradme/domain/ConsultantProfile.kt
+++ src/main/kotlin/esgi/al2/architradme/domain/ConsultantProfile.kt
@@ -4,13 +4,21 @@ import java.util.UUID
 
 class ConsultantProfile {
 
-    val id: UUID = UUID.randomUUID()
+    var id: UUID = UUID.randomUUID()
     var competences: List<String> = ArrayList()
     var tjm: Double = 0.0
     var disponibilities: List<String> = ArrayList()
     var modalities: String = ""
 
     public fun ConsultantProfile(competences: List<String>, tjm: Double, disponibilities: List<String>, modalities: String) {
+        this.competences = competences
+        this.tjm = tjm
+        this.disponibilities = disponibilities
+        this.modalities = modalities
+    }
+
+    public fun ConsultantProfile(id: UUID, competences: List<String>, tjm: Double, disponibilities: List<String>, modalities: String) {
+        this.id = id
         this.competences = competences
         this.tjm = tjm
         this.disponibilities = disponibilities
